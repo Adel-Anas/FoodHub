6abf6b9962b0cf5b4574d762657790dda936f0b3
Anas <adelanas2000@gmail.com>
2023-12-29 09:05:39 +0100

    AddedRecipe

--- src/Navbar/NavBar.js
+++ src/Navbar/NavBar.js
@@ -5,6 +5,14 @@ import { useState } from 'react';
 
 const Navbar = () => {
     const [submit, setSubmit] = useState(false);
+    const openModel = () => {
+      setSubmit(true);
+      document.body.classList.add('overflow-hidden');
+    }
+    const closeModel = () => {
+      setSubmit(false);
+      document.body.classList.remove('overflow-hidden')
+    }
     return ( 
         <>
             <div className="Navbar-Container">
@@ -16,30 +24,32 @@ const Navbar = () => {
                     <Link to='/Recipe'>Recipes</Link>
                     <Link to='/Blog'>Blog</Link>
                     <Link to='/Shop'>Shop</Link>
-                    <button onClick={() =>{setSubmit((prev)=>!prev)}}>Sumbit Recipe</button>
+                    <button onClick={openModel}>Sumbit Recipe</button>
                 </div>
             </div>
             
             {submit && (
-        <div className="recipe-container fixed bg-black-500 flex items-center justify-center	">
-          <div className="recipe-box">
-           
-              <label>
+        <div className="fixed flex items-center justify-center top-0 left-0 bottom-0 right-0 z-10 bg-[rgb(0,0,0,0.8)] overflow-y-auto " >
+          <div className="input-container flex flex-col gap-y-3 text-black bg-white p-10 rounded w-[500px] h-auto relative">
+            <p className="absolute top-2 right-5 text-2xl font-bold cursor-pointer" onClick={closeModel}>x</p>
+            <p className="text-center font-medium text-3xl">ADD A RECIPE :</p>
+              <label className='input-title'>
                 Category :
-                <select name="" id="">
+              </label>
+                <select name="" id="" className='text-black border-black border h-12' >
                   <option value="">Morrocan</option>
-                  <option value="">Italien</option>
+                  <option value="">Italian</option>
                   <option value="">Japanese</option>
                   <option value="">Turkish</option>
                   <option value="">German</option>
                 </select>
-              </label>
-              <label>Name:</label>
-              <input type='text' value="" name='Name'/>
-              <label>ingredients:</label>
-              <input type='text' value="" name='ingredients'/>
-              <label>instructions:</label>
-              <input type='text' value="" name='instructions'/>
+              <label className='input-title'>Name:</label>
+              <input type='text' name='Name' className='input'/>
+              <label className='input-title'>Ingredients:</label>
+              <textarea name="ingredients" id="" cols="10" rows="6" className='textarea'></textarea>
+              <label className='input-title'>Instructions:</label>
+              <textarea name="instructions" id="" cols="10" rows="6" className='textarea'></textarea>
+              <button className='add-recipe'><span>ADD</span></button>
            
           </div>
         </div>
