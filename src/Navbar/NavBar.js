f76a0fc2f762f0b2e12ebfd25c3f22db5ffdcb16
oumaimaezarouali <oumaima.2001ezarouali@gmail.com>
2023-12-29 09:22:14 +0100

    post change

--- src/Navbar/NavBar.js
+++ src/Navbar/NavBar.js
@@ -2,9 +2,32 @@ import '../Navbar/Navbar.css';
 import { Link } from 'react-router-dom';
 import FoodLogo from '../Images/FoodLogo.png';
 import { useState } from 'react';
+import axios from 'axios';
 
 const Navbar = () => {
     const [submit, setSubmit] = useState(false);
+    const [values , setValues] = useState({
+        category:"",
+        name:"",
+        ingredients:"",
+        instructions:""
+
+    })
+
+
+    const handleClick =(e) =>{
+       e.preventDefault()
+        axios.post("http://localhost:4000/recipes" , values)
+        .then(res =>console.log(res.data) )
+        
+        .catch(err => console.log(err))
+        setValues({
+            category:"",
+            name:"",
+            ingredients:"",
+            instructions:""
+        })
+    }
     return ( 
         <>
             <div className="Navbar-Container">
@@ -20,13 +43,16 @@ const Navbar = () => {
                 </div>
             </div>
             
+
             {submit && (
         <div className="recipe-container fixed bg-black-500 flex items-center justify-center	">
           <div className="recipe-box">
+           <form onSubmit={handleClick}>
+
            
               <label>
                 Category :
-                <select name="" id="">
+                <select name="" id=""  onChange={e=> setValues({...setValues , category:e.target.value})}>
                   <option value="">Morrocan</option>
                   <option value="">Italien</option>
                   <option value="">Japanese</option>
@@ -35,12 +61,14 @@ const Navbar = () => {
                 </select>
               </label>
               <label>Name:</label>
-              <input type='text' value="" name='Name'/>
+              <input type='text'  name='name' onChange={e => setValues({...setValues , name:e.target.value})}/>
               <label>ingredients:</label>
-              <input type='text' value="" name='ingredients'/>
+              <input type='text'  name='ingredients' onChange={e => setValues({...setValues , ingredients:e.target.value})}/>
               <label>instructions:</label>
-              <input type='text' value="" name='instructions'/>
-           
+              <input type='text'  name='instructions' onChange={e => setValues({...setValues , instructions:e.target.value})}/>
+              
+              <button>Add new recipe</button>
+           </form>
           </div>
         </div>
       )}
